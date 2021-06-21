93db4f5b8926cdc5892f483d8b6062d5f9742119
Jacques Schmitz Junior <juniorschmitz9@gmail.com>
2021-06-21 19:19:33 -0300

    add: chaining events scenarios complete

--- cypress/integration/chaining_start.js
+++ cypress/integration/chaining_start.js
@@ -10,6 +10,18 @@ it('Chaining commands', () => {
 
   cy
     .get('[data-cy="task"]')
+    .eq(1)
     .should('be.visible')
+    .should('have.text', 'milk')
+
+  // context of first element, gets the first element available on the DOM
+  cy
+    .contains('milk')
+
+  // context of the second list, we are passing the first element as context
+  cy
+    .get('data-cy="list')
+    .eq(1)
+    .contains('milk')
 
 });

50b5aa14e2c6fb36ca03e294a8bdf675efc7b864
Jacques Schmitz Junior <juniorschmitz9@gmail.com>
2021-06-21 19:07:18 -0300

    add: multiple assertions scenarios complement

--- cypress/integration/multiple_assertions_start.js
+++ cypress/integration/multiple_assertions_start.js
@@ -6,7 +6,7 @@ beforeEach(() => {
     .visit('/board/2305140181')
 })
 
-it('Multiple assertions', () => {
+it('Multiple assertions start', () => {
 
   cy
     .get('[data-cy=task]')
@@ -17,5 +17,25 @@ it('Multiple assertions', () => {
     .get('[data-cy=task]')
     .eq(1)
     .should('contain.text', 'milk')
+
+
+  // then function does not contain retry logic, that means that whatever the get command yields, then function will use
+  cy
+    .get('data-cy=task')
+    .then(item => {
+      expect(item[0]).to.contain.text('bread')
+      expect(item[1]).to.contain.text('milk')
+    })
+
+  // should function contains the retry logic (if we reorder itens during the test, it will retry until timeout or test passes)
+  cy
+  .get('data-cy=task')
+  .should(item => {
+    if(item.length !== 3) {
+      throw new Error('Not enough elements!')
+    }
+    expect(item[0]).to.contain.text('bread')
+    expect(item[1]).to.contain.text('milk')
+  })
 
 })
